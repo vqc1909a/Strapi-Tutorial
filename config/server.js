74e63f2939f5d6982fc5b2b81719128f8407f1e1
Nero Angel <neroangel1909@gmail.com>
2021-04-21 19:11:26 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1992fba6fd28b6d026352642906817d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1b48929a-dabc-42e8-a051-72aca3fb5e3d'
+};
